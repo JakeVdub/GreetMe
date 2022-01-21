dbe88cbc5df55379488e10e31436333db3c53cf3
JakeVdub <jakevandewalle@gmail.com>
2022-01-20 19:40:38 -0600

    Initial Commit

---
+++ GreetMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GreetMe
+//
+//  Created by Jacob on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GreetMe/GreetMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetMeApp.swift
+//  GreetMe
+//
+//  Created by Jacob on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GreetMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
