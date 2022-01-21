131083c15a433ecf942b9fc0a098480d0dbc1086
JakeVdub <jakevandewalle@gmail.com>
2022-01-20 21:23:16 -0600

    Wrap up chapter one part six

--- GreetMe/ContentView.swift
+++ GreetMe/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About this location:")
+                    .font(.title2)
+                
+                Text("This place is pretty cool!")
                     .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+                    .foregroundColor(.secondary)
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
