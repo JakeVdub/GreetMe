bc3e974a12994bc4f7cf45fb16889d6a0642aa4b
JakeVdub <jakevandewalle@gmail.com>
2022-01-22 09:46:34 -0600

    Finish section 5 of chapter 2

---
+++ GreetMe/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  GreetMe
+//
+//  Created by Jacob on 1/22/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
