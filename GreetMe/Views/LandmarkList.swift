5b0d7a05fa89f0b5ad40e5e7a332586b14290ce8
JakeVdub <jakevandewalle@gmail.com>
2022-01-22 09:58:53 -0600

    Finish chapter 2 of Apple's Swift tutorial

--- GreetMe/Views/LandmarkList.swift
+++ GreetMe/Views/LandmarkList.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        let devices = [
+            "iPhone SE (2nd generation)",
+            "iPhone XS Max",
+            "iPhone 12 Pro Max"
+        ]
+        
+        ForEach(devices, id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
