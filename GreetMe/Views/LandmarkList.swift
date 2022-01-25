f2e36c7b90e4a0462bfe4e5da5c1ba5a5d934515
JakeVdub <jakevandewalle@gmail.com>
2022-01-24 18:29:08 -0600

    Sections 1-3 of cahpter 3 complete

--- GreetMe/Views/LandmarkList.swift
+++ GreetMe/Views/LandmarkList.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var onlyFavorites = false
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $onlyFavorites) {
+                    Text("Display Favorites")
+                }
+                ForEach(landmarks) { landmark in
+                    if (landmark.isFavorite || !onlyFavorites) {
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -24,16 +33,17 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        let devices = [
-            "iPhone SE (2nd generation)",
-            "iPhone XS Max",
-            "iPhone 12 Pro Max"
-        ]
-        
-        ForEach(devices, id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
+//        let devices = [
+//            "iPhone SE (2nd generation)",
+//            "iPhone XS Max",
+//            "iPhone 12 Pro Max"
+//        ]
+//
+//        ForEach(devices, id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
     }
 }
